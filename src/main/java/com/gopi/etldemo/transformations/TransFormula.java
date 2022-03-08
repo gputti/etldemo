877a649c31ec07531d15202ebfed872ed97ea269
Gopi Putti <gopi@macheye.com>
2022-03-08 14:04:47 -0800

    removing unwanted info

--- src/main/java/com/gopi/etldemo/transformations/TransFormula.java
+++ src/main/java/com/gopi/etldemo/transformations/TransFormula.java
@@ -1,20 +1,4 @@
-/**
- * Copyright © 2018–2020 by MachEye Inc. All rights reserved.
- * NOTICE: All information contained herein is the property of MachEye Inc.
- * No part of this publication may be reproduced, stored in a retrieval system, 
- * or transmitted, in any form or by any means, electronic, mechanical, 
- * photocopying, or otherwise, without the prior written consent of 
- * the publisher. Any software referred to herein is furnished under 
- * license and may only be used or copied in accordance with the terms 
- * of such license.
- * This publication and the information herein is furnished AS IS, and 
- * is subject to change without notice. MachEye Inc. assumes no responsibility 
- * or liability for any errors or inaccuracies, makes no warranty of any kind 
- * (express, implied or statutory) with respect to this publication, 
- * and expressly disclaims any and all warranties of merchantability, 
- * fitness for particular purposes and noninfringement of third party rights.
- * 
- **/
+
 package com.gopi.etldemo.transformations;
 
 import java.util.List;
